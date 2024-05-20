e400b89e669d77d0bbc65513223732896d71af88
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-05-20 14:33:07 -0400

    DOC: ATT

---
+++ BD.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE myframecg;
+
+CREATE TABLE cliente (
+  cliente_id SERIAL PRIMARY KEY,
+  nome VARCHAR(255) NOT NULL UNIQUE,
+  email VARCHAR(255) DEFAULT NULL,
+  whats_app INT NOT NULL,
+  localidade VARCHAR(255) DEFAULT NULL,
+);
+
+CREATE TABLE produto(
+    produto_id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    valor DECIMAL(10,2) NOT NULL,
+    descricao VARCHAR(255) DEFAULT NULL
+)
+
+CREATE TABLE venda(
+    venda_id SERIAL PRIMARY KEY,
+    cliente_id INT NOT NULL,
+    produto_id INT NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    data DATE NOT NULL,
+    FOREIGN KEY (cliente_id) REFERENCES cliente(cliente_id),
+    FOREIGN KEY (produto_id) REFERENCES produto(produto_id)
+)
+
+CREATE TABLE despesas_venda(
+    despesas_id SERIAL PRIMARY KEY,
+    venda_id INT NOT NULL,
+    uber_flash DECIMAL(10,2) DEFAULT NULL,
+    impressao DECIMAL(10,2) DEFAULT NULL,
+    outros DECIMAL(10,2) DEFAULT NULL,
+    descricao VARCHAR(255) DEFAULT NULL,
+    valor DECIMAL(10,2) DEFAULT NULL,
+    FOREIGN KEY (venda_id) REFERENCES venda(venda_id)
+)
+
