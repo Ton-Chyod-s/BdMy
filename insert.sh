315e3ff9174e28052bd593c2f09fd2f4e1d77234
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-05-21 08:39:23 -0400

    doc: att

--- insert.sh
+++ insert.sh
@@ -1,9 +1,9 @@
-#! /bin/bash
+#! %windir%\system32\cmd.exe
 
-PSQL="psql --username=postgres --dbname=myframecg -t --no-align -c"
+PSQL="psql --username=postgres --dbname=myframecg -t --no-align"
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
 
-echo "$($PSQL "TRUNCATE TABLE cliente, produto")"
+# echo "$($PSQL "TRUNCATE TABLE cliente, produto")"
 
 # cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS; 
