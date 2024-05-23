011ae73c4077e691adc99af0bcbf0b82a01c4a37
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-05-23 10:48:10 -0400

    doc: att

--- insert.sh
+++ insert.sh
@@ -2,6 +2,8 @@
 
 PSQL="psql -U postgres -d myframecg -t --no-align"
 
+echo "$($PSQL -c 'create table if not exists cliente(nome varchar(100) DEFAULT NULL, email varchar(100) DEFAULT NULL, whats_app int DEFAULT NULL, localidade varchar(100) DEFAULT NULL);')"
+
 echo "$($PSQL -c 'create table if not exists produto(nome varchar(100), qtde integer, valor numeric(10,2), data date, entrega date);')"
 
 # echo "$($PSQL -c 'select * from produto;')"
