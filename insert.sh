e5bb9be374477dd7571b791e959e05114f555d86
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-05-23 16:50:46 -0400

    doc: att

--- insert.sh
+++ insert.sh
@@ -1,13 +1,20 @@
 PSQL="psql -U postgres -t --no-align"
 
+
 cat arquivosCSV/DESPESAS.csv | while IFS="," read -r NOME_LOJA PRODUTO QUANTIDADE VALOR DATA ENTREGA; do
-    echo "$($PSQL -c "insert into produtos (nome, quantidade, valor, data_aquisicao, descricao) values('$NOME_LOJA', $QUANTIDADE, $VALOR, '$DATA', '$PRODUTO $ENTREGA' );")"
+    if [[ $QUANTIDADE != 'QUANTIDADE' ]]
+    then
+        echo "$($PSQL -c "insert into produtos (nome, quantidade, valor, data_aquisicao, descricao) values('$NOME_LOJA', $QUANTIDADE, $VALOR, '$DATA', '$PRODUTO $ENTREGA' );")"
+    fi
 done
 
 cat arquivosCSV/VENDAS.csv | while IFS="," read -r NOME PRODUTO QUANTIDADE VALOR UBER FLASH IMPRESSAO DATA PRAZO SITUACAO STATUS OBS ; do
-    echo "$($PSQL -c "insert into clientes (nome, email, whats_app, localidade) values('$NOME', null, null, null);")"
-    CLIENTE_ID= echo "$($PSQL -c "select cliente_id from clientes where nome = '$NOME';")"
+    if [[ $QUANTIDADE != 'QUANTIDADE' ]]
+    then
+        echo "$($PSQL -c "insert into clientes (nome, email, whats_app, localidade) values('$NOME', null, null, null);")"
+        CLIENTE_ID="$($PSQL -c "select cliente_id from clientes where nome = '$NOME';")"
+        
+        echo "$($PSQL -c "insert into vendas (cliente_id, produto, status, data_aquisicao) values($CLIENTE_ID, '$PRODUTO', '$SITUACAO', '$DATA' );")"
     
-    echo "$($PSQL -c "insert into vendas (cliente_id, produto, status, data) values(1, '$PRODUTO', '$SITUACAO', '$DATA'")"
-    
+    fi
 done
