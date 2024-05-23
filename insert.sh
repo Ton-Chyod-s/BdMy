d3a884334ab1909fc8bc9a2496294ed36f089cd6
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-05-23 13:49:49 -0400

    doc: att

--- insert.sh
+++ insert.sh
@@ -1,5 +1,5 @@
 PSQL="psql -U postgres -t --no-align"
 
-cat arquivos/DESPESAS.cvs | while IFS="," read -r NOME_LOJA PRODUTO QTDE VALOR DATA ENTREGA; do
-    echo "$($PSQL -c "$SQL")"
+cat arquivosCSV/DESPESAS.csv | while IFS="," read -r NOME_LOJA PRODUTO QTDE VALOR DATA ENTREGA; do
+    echo "$($PSQL -c "insert into produtos (nome, quantidade, valor, data_aquisicao, descricao) values('$NOME_LOJA', $QTDE, $VALOR, '$DATA', '$PRODUTO $ENTREGA' );")"
 done
