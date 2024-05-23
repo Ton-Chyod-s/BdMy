a9b0b294801ab894bc9dacd2e4a95237ca7d7174
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-05-23 13:24:06 -0400

    doc: att

--- insert.sh
+++ insert.sh
@@ -1,18 +1,8 @@
 PSQL="psql -U postgres -t --no-align"
 
-echo "$($PSQL -c 'drop table if exists produto, cliente;')"
-
-CLIENTE="cliente(cliente_id SERIAL PRIMARY KEY, nome varchar(100) DEFAULT NULL, email varchar(100) DEFAULT NULL, whats_app int DEFAULT NULL, localidade varchar(100) DEFAULT NULL);"
-
-PRODUTO="produto(produto_id SERIAL PRIMARY KEY, nome varchar(100), qtde integer, valor numeric(10,2), data date, entrega date);"
-
-VENDA=""
-
-DESPESAS_VENDA=""
-
-echo "$($PSQL -c "create table if not exists $CLIENTE")"
-echo "$($PSQL -c "create table if not exists $PRODUTO")"
-
+cat BD.SQL | while IFS=";" read -r SQL; do
+    echo "$($PSQL -c "$SQL")"
+done
 
 # echo "$($PSQL -c 'select * from produto;')"
 
