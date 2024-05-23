3228aa90d0a9094adb6f6f149794d82be1f7d494
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-05-23 16:25:51 -0400

    doc: att

--- insert.sh
+++ insert.sh
@@ -4,10 +4,10 @@ cat arquivosCSV/DESPESAS.csv | while IFS="," read -r NOME_LOJA PRODUTO QUANTIDAD
     echo "$($PSQL -c "insert into produtos (nome, quantidade, valor, data_aquisicao, descricao) values('$NOME_LOJA', $QUANTIDADE, $VALOR, '$DATA', '$PRODUTO $ENTREGA' );")"
 done
 
-cat arquivosCSV/VENDAS.csv | while IFS="," read -r NOME PRODUTO QUANTIDADE VALOR UBER FLASH IMPRESSAO DATA PRAZO SITUASAO STATUS OBS ; do
+cat arquivosCSV/VENDAS.csv | while IFS="," read -r NOME PRODUTO QUANTIDADE VALOR UBER FLASH IMPRESSAO DATA PRAZO SITUACAO STATUS OBS ; do
     echo "$($PSQL -c "insert into clientes (nome, email, whats_app, localidade) values('$NOME', null, null, null);")"
     CLIENTE_ID= echo "$($PSQL -c "select cliente_id from clientes where nome = '$NOME';")"
     
-    echo "$($PSQL -c "insert into vendas (cliente_id, produto, status, data_aquisicao) values(0, '$PRODUTO', '$SITUASAO', $DATA")"
+    echo "$($PSQL -c "insert into vendas (cliente_id, produto, status, data) values(1, '$PRODUTO', '$SITUACAO', '$DATA'")"
     
 done
