ce7b48de26948ee4f18505ad9b6336dd64241f40
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-05-29 07:56:03 -0400

    DOC: ATT

--- BD.sql
+++ BD.sql
@@ -2,6 +2,6 @@ CREATE TABLE clientes (cliente_id SERIAL PRIMARY KEY, nome VARCHAR(255) NOT NULL
 
 CREATE TABLE produtos (produto_id SERIAL PRIMARY KEY, nome VARCHAR(255) NOT NULL, quantidade INT NOT NULL, valor DECIMAL(10,2) NOT NULL, data_aquisicao DATE NOT NULL, descricao VARCHAR(255) DEFAULT NULL);
 
-CREATE TABLE vendas (venda_id SERIAL PRIMARY KEY, cliente_id INT NOT NULL, produto VARCHAR(255) NOT NULL, status VARCHAR(255) NOT NULL, data_aquisicao DATE NOT NULL, FOREIGN KEY (cliente_id) REFERENCES clientes(cliente_id));
+CREATE TABLE vendas (venda_id SERIAL PRIMARY KEY, cliente_id INT NOT NULL, produto VARCHAR(255) NOT NULL, status VARCHAR(255) NOT NULL, data_aquisicao DATE NOT NULL DEFAULT NOW(), FOREIGN KEY (cliente_id) REFERENCES clientes(cliente_id));
 
 CREATE TABLE despesas_vendas (despesa_id SERIAL PRIMARY KEY, venda_id INT NOT NULL, cliente_id INT NOT NULL, transporte DECIMAL(10,2) DEFAULT NULL, impressao DECIMAL(10,2) DEFAULT NULL, outros DECIMAL(10,2) DEFAULT NULL, FOREIGN KEY (cliente_ID) REFERENCES clientes(cliente_id), FOREIGN KEY (venda_id) REFERENCES vendas(venda_id));
