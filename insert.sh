58ba3aab6e7b81b922ea37865e870551250f7d59
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-06-03 14:33:16 -0400

    doc: att

--- insert.sh
+++ insert.sh
@@ -23,7 +23,7 @@ then
 fi
 
 PSQL="psql -U postgres -d myframecg -t --no-align"
-#LISTABANCODADOS="$($PSQL -c "\dt;")"
+LISTABANCODADOS="$($PSQL -c "\dt;")"
 
 cat BD.SQL | while IFS=";" read -r SQL; do
     if [[ -z $LISTABANCODADOS ]]
@@ -32,21 +32,19 @@ cat BD.SQL | while IFS=";" read -r SQL; do
     fi
 done
 
+cat arquivosCSV/DESPESAS.csv | while IFS="," read -r NOME_LOJA PRODUTO QUANTIDADE VALOR DATA ENTREGA; do
+    if [[ $QUANTIDADE != 'QUANTIDADE' ]]
+    then
+        echo "$($PSQL -c "insert into produtos (nome, quantidade, valor, data_aquisicao, descricao) values('$NOME_LOJA', $QUANTIDADE, $VALOR, '$DATA', '$PRODUTO $ENTREGA' );")"
+    fi
+done
 
-
-# cat arquivosCSV/DESPESAS.csv | while IFS="," read -r NOME_LOJA PRODUTO QUANTIDADE VALOR DATA ENTREGA; do
-#     if [[ $QUANTIDADE != 'QUANTIDADE' ]]
-#     then
-#         echo "$($PSQL -c "insert into produtos (nome, quantidade, valor, data_aquisicao, descricao) values('$NOME_LOJA', $QUANTIDADE, $VALOR, '$DATA', '$PRODUTO $ENTREGA' );")"
-#     fi
-# done
-
-# cat arquivosCSV/VENDAS.csv | while IFS="," read -r NOME PRODUTO QUANTIDADE VALOR UBER FLASH IMPRESSAO DATA PRAZO SITUACAO STATUS OBS ; do
-#     if [[ $QUANTIDADE != 'QUANTIDADE' ]]
-#     then
-#         echo "$($PSQL -c "insert into clientes (nome, email, whats_app, localidade) values( '$NOME', null, null, null);")"
-#         CLIENTE_ID="$($PSQL -c "select cliente_id from clientes where nome = '$NOME';")"
+cat arquivosCSV/VENDAS.csv | while IFS="," read -r NOME PRODUTO QUANTIDADE VALOR UBER FLASH IMPRESSAO DATA PRAZO SITUACAO STATUS OBS ; do
+    if [[ $QUANTIDADE != 'QUANTIDADE' ]]
+    then
+        echo "$($PSQL -c "insert into clientes (nome, email, whats_app, localidade) values( '$NOME', null, null, null);")"
+        CLIENTE_ID="$($PSQL -c "select cliente_id from clientes where nome = '$NOME';")"
         
-#         echo "$($PSQL -c "insert into vendas (cliente_id, produto, status, data_aquisicao) values( $CLIENTE_ID, '$PRODUTO', '$SITUACAO', '$DATA' );")"
-#     fi
-# done
+        echo "$($PSQL -c "insert into vendas (cliente_id, produto, status, data_aquisicao) values( $CLIENTE_ID, '$PRODUTO', '$SITUACAO', '$DATA' );")"
+    fi
+done
