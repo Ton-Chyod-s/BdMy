5dcff1fb373e910600cdf3954a1f86b6362ede4b
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-06-18 13:52:09 -0400

    doc: att

---
+++ html/assets/js/script.js
@@ -0,0 +1,12 @@
+var menuSite = document.getElementById("menuSite");
+
+
+function mudarCssMenu(td) {
+    td.style.backgroundColor = "#FF0F0F";
+}
+
+
+menuSite.addEventListener("mouseover", function() {
+    mudaParaVermelho(menuSite)
+
+});
