3e3ff3e0249c5cf39eb98447fcb1f179ebe50831
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-06-18 14:11:06 -0400

    doc: att

--- html/assets/js/script.js
+++ html/assets/js/script.js
@@ -1,12 +1,22 @@
 var menuSite = document.getElementById("menuSite");
+var corpoSite = document.getElementById("corpoSite");
+var cabecalhoSite = document.getElementById("cabecalhoSite");
 
 
 function mudarCssMenu(td) {
-    td.style.backgroundColor = "#FF0F0F";
+    td.style.padding = "58px";
 }
 
+function sumirHeader(td) {
+    td.style.display = "none";
+}
 
 menuSite.addEventListener("mouseover", function() {
-    mudaParaVermelho(menuSite)
+    mudarCssMenu(menuSite);
+    
+}) ;
 
-});
+menuSite.addEventListener("scrollend", function() {
+    mudarCssMenu(menuSite);
+    sumirHeader(cabecalhoSite);
+}) ;
