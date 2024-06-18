9539c1d2ae38dcb945b0c763c100791298dac2ae
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-06-18 16:05:15 -0400

    doc: att

--- html/assets/js/script.js
+++ html/assets/js/script.js
@@ -1,10 +1,27 @@
 var menuSite = document.getElementById("menuSite");
 var corpoSite = document.getElementById("corpoSite");
 var cabecalhoSite = document.getElementById("cabecalhoSite");
+var buttonMenu = document.getElementById("buttonMenu");
+var buttonMenu1 = document.getElementById("buttonMenu1");
+var buttonMenu2 = document.getElementById("buttonMenu2");
+
+function abrirMenu(td) {
+    td.style.marginLeft = "-48px";
+    td.style.width = "138px";
+}
+
+function fecharMenu(td) {
+    td.style.width = "41px";
+    td.style.marginLeft = "0px";
+}
 
 menuSite.addEventListener("mouseenter", function() {
     menuSite.style.padding = "58px";
     menuSite.style.transition = "0.2s";
+    abrirMenu(buttonMenu);
+    abrirMenu(buttonMenu1);
+    abrirMenu(buttonMenu2);
+    
 });
 
 menuSite.addEventListener("scrol", function() {
@@ -15,4 +32,9 @@ menuSite.addEventListener("scrol", function() {
 menuSite.addEventListener("mouseleave", function() {
     menuSite.style.padding = "8px";
     menuSite.style.transition = "0.2s";
+    fecharMenu(buttonMenu);
+    fecharMenu(buttonMenu1);
+    fecharMenu(buttonMenu2);
+    
 });
+
