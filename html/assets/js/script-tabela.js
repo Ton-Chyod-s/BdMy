19bab7434c16eb3a1e73f60d5728c2fab9b710d5
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-06-21 08:25:42 -0400

    doc: att

--- html/assets/js/script-tabela.js
+++ html/assets/js/script-tabela.js
@@ -1,23 +1,28 @@
-var divTabela = document.querySelector("#id-tabela");
+function criarTabela(cabeçalho, conteudo) {
+    var divTabela = document.querySelector("#id-tabela");
 
-var tabela = document.createElement("table");
-var thead = document.createElement("thead");
-var tbody=document.createElement("tbody");
+    var tabela = document.createElement("table");
+    var thead = document.createElement("thead");
+    var tbody=document.createElement("tbody");
 
-divTabela.appendChild(tabela);
-tabela.appendChild(thead);
-thead.appendChild(tbody);
+    divTabela.appendChild(tabela);
+    tabela.appendChild(thead);
+    thead.appendChild(tbody);
 
-var tr = document.createElement("tr");
-var th = document.createElement("th");
+    var tr = document.createElement("tr");
+    var th = document.createElement("th");
 
-th.textContent = "Header";
+    th.textContent = cabeçalho;
 
-tbody.appendChild(tr);
-tr.appendChild(th);
+    tbody.appendChild(tr);
+    tr.appendChild(th);
 
-var trBody = document.createElement("tr");
-var td = document.createElement("td");
-td.textContent = "Cell Content";
-trBody.appendChild(td);
-tbody.appendChild(trBody);
+    var trBody = document.createElement("tr");
+    var td = document.createElement("td");
+
+    td.textContent = conteudo;
+
+    trBody.appendChild(td);
+    tbody.appendChild(trBody);
+
+}
